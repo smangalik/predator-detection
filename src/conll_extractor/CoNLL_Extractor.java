1619d7a677182f3177bc4ad8743829f49aa188c9
sid <sid@sid-Q550LF>
2016-10-07 00:52:34 -0400

    Added use of the pipeline Program now uses the CoreNLP to annotate its tokens and furthermore can now return lemmas

--- src/conll_extractor/CoNLL_Extractor.java
+++ src/conll_extractor/CoNLL_Extractor.java
@@ -1,63 +1,80 @@
 package conll_extractor;
 
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.process.CoreLabelTokenFactory;
-import edu.stanford.nlp.process.PTBTokenizer;
-import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.util.StringUtils;
+
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class CoNLL_Extractor {
-
+    
     public static void main(String args[]) throws ClassNotFoundException, IOException {
-
-        /** the chat lines in a selected file as Strings */
+        
+        // Properties of the pipeline
+        Properties props = new Properties();
+        props.put("annotators", "tokenize, ssplit, pos, lemma");
+        
+        // StanfordCoreNLP
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        
+        /** the chat lines in a selected file as Strings   */
         ArrayList<String> posts = FileCleaner.cleanFile();
-
-        /** the resulting n-grams as Strings */
-        ArrayList<String> engrams = new ArrayList<>();
-
-        /** the resulting tokens as Strings */
-        ArrayList<String> tokens = new ArrayList<>();
-
-        /** the tagged Strings */
-        ArrayList<String> taggedStrings = new ArrayList<>();
-
-        /** the "n" of the n-gram */
+        
+        /** the n-grams in the post   */
+        ArrayList<String> engrams = FileCleaner.cleanFile();
+        
+        /**
+         * the "n" of the n-gram
+         */
         int n = 3;
-
+        
         // extract n-grams from posts
-        for(String post : posts){
-                ArrayList<String> words = (ArrayList<String>) StringUtils.getNgramsString(post, 1, 1);
-                ArrayList<String> ngrams = (ArrayList<String>) StringUtils.getNgrams(words, n, n);
-                for (String gram : ngrams){
-                        engrams.add(gram);
-                        //System.out.println(gram);
-                }
+        for (String post : posts) {
+            ArrayList<String> words = (ArrayList<String>) StringUtils.getNgramsString(post, 1, 1);
+            ArrayList<String> ngrams = (ArrayList<String>) StringUtils.getNgrams(words, n, n);
+            for (String gram : ngrams) {
+                engrams.add(gram);
+                //System.out.println(gram);
+            }
         }
-
-        // tokenize string in the file
-        for (String post : posts){
-            PTBTokenizer<CoreLabel> ptbt = new PTBTokenizer<>(new StringReader(post), 
-                new CoreLabelTokenFactory(), "");
-            while (ptbt.hasNext()) {
-                CoreLabel label = ptbt.next();
-                tokens.add(label.toString());
-                //System.out.println(label);
+        
+        // extract tokens, POS, and lemmas
+        for (String post : posts) {
+            Annotation document = new Annotation(post);
+            pipeline.annotate(document);
+            List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+            for(CoreMap sentence: sentences) {
+                // traversing the words in the current sentence
+                // a CoreLabel is a CoreMap with additional token-specific methods
+                int num = 0;
+                for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+                    //this is the number of the token
+                    num++;
+                    // this is the text of the token
+                    String word = token.get(TextAnnotation.class);
+                    // this is the POS tag of the token
+                    String pos = token.get(PartOfSpeechAnnotation.class);
+                    // this is the lemma label of the token
+                    String lemma = token.lemma();
+                    
+                    System.out.println(num + " " + 
+                            word + " " +
+                            pos + " " + 
+                            lemma);
+                }
             }
-        }     
-
-
-        //extract n-grams from posts
-        MaxentTagger tagger = new MaxentTagger("taggers/left3words-wsj-0-18.tagger");
-            for(String post : posts){
-                taggedStrings.add(tagger.tagString(post));
-            }				
-            for(String tagged : taggedStrings){
-                System.out.println(tagged);
-            }
+        }
     }
-	
+    
+    
 }
